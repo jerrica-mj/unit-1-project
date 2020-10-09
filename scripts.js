d76282e1d1d71d13d7580e50986740679e55ae55
jerrica-mj <69536204+jerrica-mj@users.noreply.github.com>
2020-10-09 09:03:51 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,3 @@
+// Pop-Up message on open before showing web page
+// syntax: alert(message); -- string type parameters & no return value
+alert("You are now entering the Government E-Services Portal.");
